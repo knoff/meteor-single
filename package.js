96d050ce575918b24d2ab900ee1c2715c81ae2ff
Письменов Илья <knoff@anapaplus.ru>
2014-09-07 19:49:21 +0400

    remove wrong commited data

--- package.js
+++ package.js
@@ -17,10 +17,7 @@ Package.onUse(function(api) {
   api.addFiles('client/templates/template.html','client');
   api.addFiles('client/templates/template.js','client');
   api.addFiles('client/templates/single-page.html','client');
-<<<<<<< HEAD
   api.addFiles('client/templates/single-404.html','client');
-=======
->>>>>>> origin/master
   api.addFiles('client/template-helpers.js','client');
   api.export('SingleItems',['client','server']);
   api.export('getDescendantProp',['client','server']);
