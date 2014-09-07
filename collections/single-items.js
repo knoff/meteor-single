37925ebe35e627334581a1d435283a137c994b81
Письменов Илья <knoff@anapaplus.ru>
2014-09-07 22:51:55 +0400

    Ready for v0.1.0

--- collections/single-items.js
+++ collections/single-items.js
@@ -1,1 +1,11 @@
 SingleItems = new Meteor.Collection('single-items');
+SingleItems.validation({
+	property: ["relations._id","relations.url"],
+	unique: true
+});
+SingleItems.before.insert(function(userId,doc){
+	return SingleItems.valid(doc);
+});
+SingleItems.before.update(function(userId,doc){
+	return SingleItems.valid(doc);
+});
--- package.js
+++ package.js
@@ -11,9 +11,9 @@ Package.onUse(function(api) {
   api.use('matb33:collection-hooks@0.7.3');
   api.use('jquery',['client']);
   api.use('underscore', ['client', 'server']);
-  api.addFiles('lib/validation.js');
-  api.addFiles('collections/single-items.js');
-  api.addFiles('lib/router.js');
+  api.addFiles('lib/validation.js',['client','server']);
+  api.addFiles('collections/single-items.js',['client','server']);
+  api.addFiles('lib/router.js',['client','server']);
   api.addFiles('client/templates/template.html','client');
   api.addFiles('client/templates/template.js','client');
   api.addFiles('client/templates/single-page.html','client');
