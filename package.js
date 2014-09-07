70f9da23fb53920386c35d90a2ece35aa9390e14
Ilia V. Pismenov <knoff@anapaplus.ru>
2014-09-07 19:41:06 +0400

    wrong commit

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   api.addFiles('client/templates/template.html','client');
   api.addFiles('client/templates/template.js','client');
   api.addFiles('client/templates/single-page.html','client');
-  api.addFiles('client/templates/single-page.js','client');
+  api.addFiles('client/templates/single-404.html','client');
   api.addFiles('client/template-helpers.js','client');
   api.export('SingleItems',['client','server']);
 });
