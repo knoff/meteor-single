b6cdac4b7c9596260083860893361c2c9956a1c2
Письменов Илья <knoff@anapaplus.ru>
2014-09-09 15:00:04 +0400

    Textarea editor

--- client/UI.js
+++ client/UI.js
@@ -14,35 +14,52 @@ Meteor.startup(function(){
 		'click [single-edit]': function(event){
 			var element = $(event.currentTarget);
 			switch(element.attr("single-type")){
+				case "textarea" :
+					var editor = $('<textarea class="single-editor"></textarea>');
+//					var lastScroll=0;
+					editor.bind("keyup",function(){
+//						lastScroll = (editor[0].scrollHeight>lastScroll)?editor[0].scrollHeight:lastScroll;
+						var height=(editor.val().split("\n").length*parseFloat(editor.css("line-height")));
+						editor.css({height: height+"px" });
+					})
+				break;
 				default :
 				  var editor = $('<input type="text" class="single-editor"/>');
-				  editor.val(Single.Utils.getDescendantProp(Blaze.getData(event.currentTarget),$(event.currentTarget).attr('single-edit')))
-				  editor.copyCSS(element);
-				  editor.css({border: "#ccc 1px solid"});
-				  editor.bind("focusout",function(){
-				  	var val = editor.val();
-				  	element.css({display:"block"});
-				  	editor.remove();
-				  	var found=Single.Utils.getDescendantSingleItem(
-				  			Blaze.getData(event.currentTarget),
-				  			$(event.currentTarget).attr('single-edit')
-				  		);
-				  	if(!_.isUndefined(found.found)){
-				  		Single.Utils.setDescendantProp(
-				  			Blaze.getData(event.currentTarget),
-				  			$(event.currentTarget).attr('single-edit'),
-				  			val
-				  		);
-				  		var newVal={};
-				  		newVal[found.name]=val;
-				  		SingleItems.update({_id:found.found._id},{$set:newVal});
-				  	}
-				  })
-				  //console.log(editor);
-					element.after(editor);
-					element.css({display:"none"});
 				break;
 			}
+			var val = Single.Utils.getDescendantProp(Blaze.getData(event.currentTarget),$(event.currentTarget).attr('single-edit'));
+			val = val.replace(/\n\n/g,"\n");
+			editor.val(val);
+		  editor.copyCSS(element);
+		  editor.css({border: "#ccc 1px solid"});
+		  editor.bind("focusout",function(){
+		  	var val = editor.val();
+		  	val = val.replace(/\n/g,"\n\n");
+		  	element.css({display:"block"});
+		  	editor.remove();
+		  	var found=Single.Utils.getDescendantSingleItem(
+		  			Blaze.getData(event.currentTarget),
+		  			$(event.currentTarget).attr('single-edit')
+		  		);
+		  	if(!_.isUndefined(found.found)){
+		  		Single.Utils.setDescendantProp(
+		  			Blaze.getData(event.currentTarget),
+		  			$(event.currentTarget).attr('single-edit'),
+		  			val
+		  		);
+		  		var newVal={};
+		  		newVal[found.name]=val;
+		  		SingleItems.update({_id:found.found._id},{$set:newVal});
+		  	}
+		  });
+		  switch(element.attr("single-type")){
+				default :
+				  //var editor = $('<input type="text" class="single-editor"/>');
+				break;
+			}
+		  //console.log(editor);
+			element.after(editor);
+			element.css({display:"none"});
 			//$(event.CurrentTarget).after()
 			//console.log($(event.currentTarget).attr('single-edit'));
 			//console.log(Single.Utils.getDescendantProp(Blaze.getData(event.currentTarget),$(event.currentTarget).attr('single-edit')));
