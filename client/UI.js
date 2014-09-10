274686157203ad496a6a476e43a6cece0c41ed59
Письменов Илья <knoff@anapaplus.ru>
2014-09-10 23:06:32 +0400

    fields begin

--- client/UI.js
+++ client/UI.js
@@ -12,86 +12,36 @@ Single.UI.addSchema=function(name,schema){
 Meteor.startup(function(){
 	Template['single-page'].events={
 		'click [single-edit]': function(event){
-			var editor = $(event.currentTarget);
-			console.log((editor.attr('contenteditable')==="true"));
+			var view = $(event.currentTarget);
+			var editor = view.clone();
+			view.after(editor);
+			var display = view.css("display");
+			view.css({display:"none"});
+			editor.removeAttr("single-edit").removeAttr("single-type");
 			if(!(editor.attr('contenteditable')==="true")){
 				editor.attr('contenteditable',true);
 				editor.blur(function(event){
-					console.log(editor.html());
 					editor.attr('contenteditable',false);
 					var found=Single.Utils.getDescendantSingleItem(
-		  			Blaze.getData(event.currentTarget),
-		  			$(event.currentTarget).attr('single-edit')
+		  			Blaze.getData(view[0]),
+		  		view.attr('single-edit')
 		  		);
 		  		var val = editor.html();
 					if(!_.isUndefined(found.found)){
 			  		Single.Utils.setDescendantProp(
-			  			Blaze.getData(event.currentTarget),
-			  			$(event.currentTarget).attr('single-edit'),
+			  			Blaze.getData(view[0]),
+			  			view.attr('single-edit'),
 			  			val
 			  		);
 			  		var newVal={};
 			  		newVal[found.name]=val;
 			  		SingleItems.update({_id:found.found._id},{$set:newVal});
+			  		editor.remove();
+			  		view.css({display:display});
 		  		}
 				})
 			}
-			/*
-			var element = $(event.currentTarget);
-			switch(element.attr("single-type")){
-				case "textarea" :
-					var editor = $('<textarea class="single-editor"></textarea>');
-//					var lastScroll=0;
-					editor.bind("keyup",function(){
-//						lastScroll = (editor[0].scrollHeight>lastScroll)?editor[0].scrollHeight:lastScroll;
-						var height=(editor.val().split("\n").length*parseFloat(editor.css("line-height")));
-						editor.css({height: height+"px" });
-					})
-				break;
-				default :
-				  var editor = $('<input type="text" class="single-editor"/>');
-				break;
-			}
-			var val = Single.Utils.getDescendantProp(Blaze.getData(event.currentTarget),$(event.currentTarget).attr('single-edit'));
-			val = val.replace(/\n\n/g,"\n");
-			editor.val(val);
-		  editor.copyCSS(element);
-		  editor.css({border: "#ccc 1px solid"});
-		  editor.bind("focusout",function(){
-		  	var val = editor.val();
-		  	val = val.replace(/\n/g,"\n\n");
-		  	element.css({display:"block"});
-		  	editor.remove();
-		  	var found=Single.Utils.getDescendantSingleItem(
-		  			Blaze.getData(event.currentTarget),
-		  			$(event.currentTarget).attr('single-edit')
-		  		);
-		  	if(!_.isUndefined(found.found)){
-		  		Single.Utils.setDescendantProp(
-		  			Blaze.getData(event.currentTarget),
-		  			$(event.currentTarget).attr('single-edit'),
-		  			val
-		  		);
-		  		var newVal={};
-		  		newVal[found.name]=val;
-		  		SingleItems.update({_id:found.found._id},{$set:newVal});
-		  	}
-		  });
-		  switch(element.attr("single-type")){
-				default :
-				  //var editor = $('<input type="text" class="single-editor"/>');
-				break;
-			}
-		  //console.log(editor);
-			element.after(editor);
-			element.css({display:"none"});
-			//$(event.CurrentTarget).after()
-			//console.log($(event.currentTarget).attr('single-edit'));
-			//console.log(Single.Utils.getDescendantProp(Blaze.getData(event.currentTarget),$(event.currentTarget).attr('single-edit')));
-			//var object = Blaze.getData(event.currentTarget);
-			//Single.Utils.setDescendantProp(object,$(event.currentTarget).attr('single-edit'),"test")
-			//console.log(Single.Utils.getDescendantProp(Blaze.getData(event.currentTarget),$(event.currentTarget).attr('single-edit')));
-			*/
+			editor.focus();
 		}
 	}
 });
